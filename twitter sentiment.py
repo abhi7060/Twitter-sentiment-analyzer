c0141fc8928fa397a15c8da100ba861010e9fcfb
abhi7060 <abhisheksharma7060@gmail.com>
2017-07-27 15:22:55 +0530

    Update twitter sentiment.py

--- twitter sentiment.py
+++ twitter sentiment.py
@@ -2,10 +2,10 @@
 import tweepy
 from textblob import TextBlob
  #keys and tokens from te twitter dev console
-consumer_key='Yf9snreZtr04HVWNIIPNH71lG'
-consumer_secret='DT8SDM69cDUsZqGjrmGJTA1oRv7YhtzxBD3EP9KxY0FpJ7TMiJ'
-access_token = '869741229331841025-zcPZRTeeicPC5VYw9tSfpV6aN1lWzJf'
-access_token_secret = 'abfZvxhKqroxuz9BsU7tXTDIONVAfXa6uj3eal185AoOV'     
+consumer_key='xxxxxxxxxxxxxx'
+consumer_secret='xxxxxxxxxxxxxxxxxxxxxxxx'
+access_token = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'     
 #attempting authentication with twitter
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
